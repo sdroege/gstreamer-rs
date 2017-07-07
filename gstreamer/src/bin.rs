decf5da6d011ab63e8ef61b06fdbb0635c3d98ec
Sebastian Dröge <sebastian@centricular.com>
2017-07-07 14:47:28 +0300

    Add copyright notices

--- gstreamer/src/bin.rs
+++ gstreamer/src/bin.rs
@@ -1,3 +1,11 @@
+// Copyright (C) 2017 Sebastian Dröge <sebastian@centricular.com>
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use Bin;
 use Element;
 
