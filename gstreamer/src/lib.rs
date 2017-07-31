ff12c054e32700001c18748cfd868a281208a857
Sebastian Dröge <sebastian@centricular.com>
2017-07-31 11:32:36 +0100

    Add all tags from core

--- gstreamer/src/lib.rs
+++ gstreamer/src/lib.rs
@@ -6,6 +6,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+#![recursion_limit="256"]
 #[macro_use]
 extern crate bitflags;
 extern crate libc;
