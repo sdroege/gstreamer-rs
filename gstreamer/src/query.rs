963557b79f5d3a91db944ed953a8c9fcab78a6ba
Sebastian Dröge <sebastian@centricular.com>
2017-12-06 13:43:37 +0200

    Query::get_structure() can also return NULL

--- gstreamer/src/query.rs
+++ gstreamer/src/query.rs
@@ -120,10 +120,14 @@ impl GstRc<QueryRef> {
 }
 
 impl QueryRef {
-    pub fn get_structure(&self) -> &StructureRef {
+    pub fn get_structure(&self) -> Option<&StructureRef> {
         unsafe {
             let structure = ffi::gst_query_get_structure(self.as_mut_ptr());
-            StructureRef::from_glib_borrow(structure)
+            if structure.is_null() {
+                None
+            } else {
+                Some(StructureRef::from_glib_borrow(structure))
+            }
         }
     }
 
