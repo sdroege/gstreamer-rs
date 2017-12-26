d44869711a958b2e9fe479e4dc0e1c56c2753cc0
Sebastian Dröge <sebastian@centricular.com>
2017-12-26 18:18:13 +0100

    Run video_rectangle.rs through rustfmt

--- gstreamer-video/src/video_rectangle.rs
+++ gstreamer-video/src/video_rectangle.rs
@@ -18,16 +18,33 @@ pub struct VideoRectangle {
 
 impl VideoRectangle {
     pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
-        VideoRectangle {
-            x, y, w, h
-        }
+        VideoRectangle { x, y, w, h }
     }
 }
 
-pub fn center_video_rectangle(src: VideoRectangle, dst: VideoRectangle, scale: bool) -> VideoRectangle {
-    let mut result = ffi::GstVideoRectangle { x: 0, y: 0, w: 0, h: 0 };
-    let src_rect = ffi::GstVideoRectangle { x: src.x, y: src.y, w: src.w, h: src.h };
-    let dst_rect = ffi::GstVideoRectangle { x: dst.x, y: dst.y, w: dst.w, h: dst.h };
+pub fn center_video_rectangle(
+    src: VideoRectangle,
+    dst: VideoRectangle,
+    scale: bool,
+) -> VideoRectangle {
+    let mut result = ffi::GstVideoRectangle {
+        x: 0,
+        y: 0,
+        w: 0,
+        h: 0,
+    };
+    let src_rect = ffi::GstVideoRectangle {
+        x: src.x,
+        y: src.y,
+        w: src.w,
+        h: src.h,
+    };
+    let dst_rect = ffi::GstVideoRectangle {
+        x: dst.x,
+        y: dst.y,
+        w: dst.w,
+        h: dst.h,
+    };
     unsafe {
         ffi::gst_video_sink_center_rect(src_rect, dst_rect, &mut result, scale as i32);
     }
