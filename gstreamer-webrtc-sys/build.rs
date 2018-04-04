a9a4608562bcd377f116d5d4806bff439eca5168
Sebastian Dröge <sebastian@centricular.com>
2018-04-04 10:27:54 +0300

    Fix doubled Gst dependency and link library for gstreamer-webrtc-sys

--- gstreamer-webrtc-sys/build.rs
+++ gstreamer-webrtc-sys/build.rs
@@ -14,8 +14,8 @@ fn main() {
 }
 
 fn find() -> Result<(), Error> {
-    let package_name = "gstreamer-player-1.0";
-    let shared_libs = ["gstplayer-1.0"];
+    let package_name = "gstreamer-webrtc-1.0";
+    let shared_libs = ["gstwebrtc-1.0"];
     let version = "1.14";
 
     if let Ok(lib_dir) = env::var("GTK_LIB_DIR") {
