9af672705457a28475d191f6e5c4adf65a81e77c
Thibault Saunier <tsaunier@igalia.com>
2018-10-31 13:10:30 -0300

    Generate bindings for the GStreamer Editing Services

---
+++ examples/src/bin/ges.rs
@@ -0,0 +1,95 @@
+extern crate gstreamer as gst;
+use gst::prelude::*;
+
+extern crate gstreamer_editing_services as ges;
+use ges::prelude::*;
+use ges::PipelineExt;
+
+use std::env;
+
+#[allow(unused_imports)]
+#[path = "../examples-common.rs"]
+mod examples_common;
+
+extern crate failure;
+#[allow(unused_imports)]
+use failure::Error;
+
+extern crate glib;
+
+fn main_loop(uri: &str) -> Result<(), glib::BoolError> {
+    ges::init()?;
+
+    let timeline = ges::Timeline::new_audio_video();
+    let layer = timeline.append_layer();
+    let pipeline = ges::Pipeline::new();
+    pipeline.set_timeline(&timeline);
+
+    let clip = ges::UriClip::new(uri);
+    layer.add_clip(&clip);
+
+    let effect = ges::Effect::new("agingtv");
+    clip.add(&effect).unwrap();
+
+
+    println!("Agingtv scratch-lines: {}",
+        clip.get_child_property("scratch-lines").unwrap().serialize().unwrap());
+
+    let asset = clip.get_asset().unwrap();
+    let duration = asset.downcast::<ges::UriClipAsset>().unwrap().get_duration();
+    println!("Clip duration: {} - playing file from {} for {}", duration,
+        duration / 2, duration / 4);
+
+    clip.set_inpoint(duration / 2);
+    clip.set_duration(duration / 4);
+
+    let ret = pipeline.set_state(gst::State::Playing);
+    assert_ne!(ret, gst::StateChangeReturn::Failure);
+
+    let bus = pipeline.get_bus().unwrap();
+    while let Some(msg) = bus.timed_pop(gst::CLOCK_TIME_NONE) {
+        use gst::MessageView;
+
+        match msg.view() {
+            MessageView::Eos(..) => break,
+            MessageView::Error(err) => {
+                println!(
+                    "Error from {:?}: {} ({:?})",
+                    err.get_src().map(|s| s.get_path_string()),
+                    err.get_error(),
+                    err.get_debug()
+                );
+                break;
+            }
+            _ => (),
+        }
+    }
+
+    let ret = pipeline.set_state(gst::State::Null);
+    assert_ne!(ret, gst::StateChangeReturn::Failure);
+
+    Ok(())
+}
+
+#[allow(unused_variables)]
+fn example_main() {
+    let args: Vec<_> = env::args().collect();
+    let uri: &str = if args.len() == 2 {
+        args[1].as_ref()
+    } else {
+        println!("Usage: ges launch");
+        std::process::exit(-1)
+    };
+
+    match main_loop(uri) {
+        Ok(r) => r,
+        Err(e) => eprintln!("Error! {}", e),
+    }
+
+}
+
+fn main() {
+    // tutorials_common::run is only required to set up the application environent on macOS
+    // (but not necessary in normal Cocoa applications where this is set up autmatically)
+    examples_common::run(example_main);
+}
---
+++ gstreamer-editing-services/src/lib.rs
@@ -0,0 +1,88 @@
+// Copyright (C) 2017 Sebastian Dröge <sebastian@centricular.com>
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+extern crate libc;
+
+use std::sync::{Once, ONCE_INIT};
+
+extern crate glib_sys as glib_ffi;
+extern crate gobject_sys as gobject_ffi;
+extern crate gio_sys as gio_ffi;
+extern crate gstreamer as gst;
+extern crate gstreamer_sys as gst_ffi;
+extern crate gstreamer_base as gst_base;
+extern crate gstreamer_base_sys as gst_base_ffi;
+extern crate gstreamer_pbutils_sys as gst_pbutils_ffi;
+extern crate gstreamer_pbutils as gst_pbutils;
+extern crate gstreamer_editing_services_sys as ffi;
+
+use glib::translate::{from_glib};
+
+#[macro_use]
+extern crate glib;
+extern crate gio;
+
+static GES_INIT: Once = ONCE_INIT;
+
+pub use glib::{Cast, Continue, Error, BoolError, IsA, StaticType, ToValue, Type, TypedValue, Value};
+
+pub fn init() -> Result<(), BoolError> {
+    if gst::init().is_err() {
+        return Err(BoolError("Could not initialize GStreamer."));
+    }
+
+    unsafe {
+        if from_glib(ffi::ges_init()) {
+            Ok(())
+        } else {
+            Err(BoolError("Could not initialize GES."))
+        }
+    }
+}
+
+pub unsafe fn deinit() {
+    ffi::ges_deinit();
+}
+
+macro_rules! assert_initialized_main_thread {
+    () => {
+        if unsafe { ::gst_ffi::gst_is_initialized() } != ::glib_ffi::GTRUE {
+            panic!("GStreamer has not been initialized. Call `gst::init` first.");
+        }
+        ::GES_INIT.call_once(|| {
+            unsafe { ::ffi::ges_init() };
+        });
+    };
+}
+
+macro_rules! skip_assert_initialized {
+    () => {};
+}
+
+#[cfg_attr(feature = "cargo-clippy", allow(unreadable_literal))]
+#[cfg_attr(feature = "cargo-clippy", allow(transmute_ptr_to_ref))]
+#[cfg_attr(feature = "cargo-clippy", allow(too_many_arguments))]
+#[cfg_attr(feature = "cargo-clippy", allow(match_same_arms))]
+mod auto;
+pub use auto::*;
+
+#[macro_use]
+extern crate bitflags;
+
+mod timeline_element;
+pub use timeline_element::TimelineElementExtManual;
+
+// Re-export all the traits in a prelude module, so that applications
+// can always "use gst::prelude::*" without getting conflicts
+pub mod prelude {
+    pub use glib::prelude::*;
+    pub use gst::prelude::*;
+    pub use timeline_element::TimelineElementExtManual;
+
+    pub use auto::traits::*;
+}
