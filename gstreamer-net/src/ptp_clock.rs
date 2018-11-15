533e4af669607fe9d0aba287f8ee44d2982a0212
Christian Meissl <meissl.christian@gmail.com>
2018-11-15 16:19:54 +0100

    net: Actually create an PTP clock instead of NTP clock

--- gstreamer-net/src/ptp_clock.rs
+++ gstreamer-net/src/ptp_clock.rs
@@ -14,36 +14,21 @@ use glib::translate::*;
 use gst;
 
 impl PtpClock {
-    pub fn new<'a, P: Into<Option<&'a str>>>(
-        name: P,
-        remote_address: &str,
-        remote_port: i32,
-        base_time: gst::ClockTime,
-    ) -> PtpClock {
+    pub fn new<'a, P: Into<Option<&'a str>>>(name: P, domain: u32) -> PtpClock {
         assert_initialized_main_thread!();
         let name = name.into();
         let name = name.to_glib_none();
         let (major, minor, _, _) = gst::version();
         if (major, minor) > (1, 12) {
             unsafe {
-                gst::Clock::from_glib_full(ffi::gst_ntp_clock_new(
-                    name.0,
-                    remote_address.to_glib_none().0,
-                    remote_port,
-                    base_time.to_glib(),
-                ))
-                .downcast_unchecked()
+                gst::Clock::from_glib_full(ffi::gst_ptp_clock_new(name.0, domain))
+                    .downcast_unchecked()
             }
         } else {
             // Workaround for bad floating reference handling in 1.12. This issue was fixed for 1.13
             unsafe {
-                gst::Clock::from_glib_none(ffi::gst_ntp_clock_new(
-                    name.0,
-                    remote_address.to_glib_none().0,
-                    remote_port,
-                    base_time.to_glib(),
-                ))
-                .downcast_unchecked()
+                gst::Clock::from_glib_none(ffi::gst_ptp_clock_new(name.0, domain))
+                    .downcast_unchecked()
             }
         }
     }
