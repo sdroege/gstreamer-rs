ee014a6df20d13c277c1bcf14d293853055becd4
Mathieu Duponchelle <mathieu@centricular.com>
2019-08-13 19:12:09 +0200

    Generate initial bindings for gstreamer-rtp
    This mostly wraps enums, flags, and constants, will wrap RTPBuffer
    next

---
+++ gstreamer-rtp/src/lib.rs
@@ -0,0 +1,51 @@
+// Copyright (C) 2018 Sebastian Dröge <sebastian@centricular.com>
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#[macro_use]
+extern crate bitflags;
+#[macro_use]
+extern crate lazy_static;
+
+#[allow(unused_imports)]
+#[macro_use]
+extern crate glib;
+extern crate glib_sys;
+extern crate gobject_sys;
+extern crate gstreamer as gst;
+extern crate gstreamer_rtp_sys as gst_rtp_sys;
+extern crate gstreamer_sys as gst_sys;
+
+macro_rules! assert_initialized_main_thread {
+    () => {
+        if unsafe { ::gst_sys::gst_is_initialized() } != ::glib_sys::GTRUE {
+            panic!("GStreamer has not been initialized. Call `gst::init` first.");
+        }
+    };
+}
+
+macro_rules! skip_assert_initialized {
+    () => {};
+}
+
+pub use glib::{Cast, Continue, Error, IsA, StaticType, ToValue, Type, TypedValue, Value};
+
+#[allow(clippy::unreadable_literal)]
+#[allow(clippy::too_many_arguments)]
+#[allow(clippy::match_same_arms)]
+#[allow(non_snake_case)]
+mod auto;
+pub use auto::*;
+
+// Re-export all the traits in a prelude module, so that applications
+// can always "use gst::prelude::*" without getting conflicts
+pub mod prelude {
+    pub use glib::prelude::*;
+    pub use gst::prelude::*;
+
+    pub use auto::traits::*;
+}
