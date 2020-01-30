6263922b6d16b4abb7fc0193ad1c72c6c268cb5b
Valmir Pretto <valmir.pretto@deepx.it>
2020-01-30 09:30:17 -0300

    gstreamer: BusStream tests

--- gstreamer/src/lib.rs
+++ gstreamer/src/lib.rs
@@ -48,6 +48,9 @@ extern crate serde_bytes;
 #[macro_use]
 extern crate serde_derive;
 
+#[cfg(test)]
+extern crate futures_executor;
+
 use glib::translate::{from_glib, from_glib_full};
 
 macro_rules! assert_initialized_main_thread {
