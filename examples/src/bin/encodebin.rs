6fddee5df92846dfcff3829957ab57c60afdb482
Vikram Fugro <vikram.fugro@gmail.com>
2020-04-01 16:06:23 +0200

    examples: remove the use of description() in Error
    Replace the use of deprecated method "Error::description()"
    with to_string() method

--- examples/src/bin/encodebin.rs
+++ examples/src/bin/encodebin.rs
@@ -316,7 +316,7 @@ fn example_main() -> Result<(), Error> {
                             .get_src()
                             .map(|s| String::from(s.get_path_string()))
                             .unwrap_or_else(|| String::from("None")),
-                        error: err.get_error().description().into(),
+                        error: err.get_error().to_string(),
                         debug: err.get_debug(),
                         cause: err.get_error(),
                     }
