6db70eaa4bb06288de3708b738ec2267fba94951
Guillaume Gomez <guillaume1.gomez@gmail.com>
2020-04-11 22:21:55 +0200

    gstreamer-editing-services: Move Asset::request_async method into manual code to add missing init assert
    See https://github.com/gtk-rs/gir/issues/898

--- gstreamer-editing-services/src/auto/asset.rs
+++ gstreamer-editing-services/src/auto/asset.rs
@@ -60,61 +60,6 @@ impl Asset {
             }
         }
     }
-
-    pub fn request_async<
-        P: IsA<gio::Cancellable>,
-        Q: FnOnce(Result<Asset, glib::Error>) + Send + 'static,
-    >(
-        extractable_type: glib::types::Type,
-        id: &str,
-        cancellable: Option<&P>,
-        callback: Q,
-    ) {
-        assert_initialized_main_thread!();
-        let user_data: Box_<Q> = Box_::new(callback);
-        unsafe extern "C" fn request_async_trampoline<
-            Q: FnOnce(Result<Asset, glib::Error>) + Send + 'static,
-        >(
-            _source_object: *mut gobject_sys::GObject,
-            res: *mut gio_sys::GAsyncResult,
-            user_data: glib_sys::gpointer,
-        ) {
-            let mut error = ptr::null_mut();
-            let ret = ges_sys::ges_asset_request_finish(res, &mut error);
-            let result = if error.is_null() {
-                Ok(from_glib_full(ret))
-            } else {
-                Err(from_glib_full(error))
-            };
-            let callback: Box_<Q> = Box_::from_raw(user_data as *mut _);
-            callback(result);
-        }
-        let callback = request_async_trampoline::<Q>;
-        unsafe {
-            ges_sys::ges_asset_request_async(
-                extractable_type.to_glib(),
-                id.to_glib_none().0,
-                cancellable.map(|p| p.as_ref()).to_glib_none().0,
-                Some(callback),
-                Box_::into_raw(user_data) as *mut _,
-            );
-        }
-    }
-
-    pub fn request_async_future(
-        extractable_type: glib::types::Type,
-        id: &str,
-    ) -> Pin<Box_<dyn std::future::Future<Output = Result<Asset, glib::Error>> + 'static>> {
-        let id = String::from(id);
-        Box_::pin(gio::GioFuture::new(&(), move |_obj, send| {
-            let cancellable = gio::Cancellable::new();
-            Self::request_async(extractable_type, &id, Some(&cancellable), move |res| {
-                send.resolve(res);
-            });
-
-            cancellable
-        }))
-    }
 }
 
 pub const NONE_ASSET: Option<&Asset> = None;
