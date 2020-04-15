99f91167af1141264d4b3a7c0742f52ed3426f6c
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2020-04-15 15:12:22 +0200

    ci: build GStreamer instead of importing tarball
    Makes it much easier to update to a new gst version.

--- ci/install-gst.sh
+++ ci/install-gst.sh
@@ -1,2 +1,8 @@
-curl -L https://people.freedesktop.org/~slomo/gstreamer-1.16.1.tar.xz | tar xJC /usr/local
-sed -i "s;prefix=/root/gstreamer;prefix=/usr/local/gstreamer;g" /usr/local/gstreamer/lib/x86_64-linux-gnu/pkgconfig/*.pc
+pip3 install meson==0.54.0
+
+git clone --depth 1 https://gitlab.freedesktop.org/gstreamer/gst-build.git --branch 1.16.2
+cd gst-build
+
+meson build -D prefix=/usr/local -D devtools=disabled -D examples=disabled -D gtk_doc=disabled -D introspection=disabled -D libav=disabled -D libnice=disabled -D python=disabled -D ugly=disabled -D vaapi=disabled
+ninja -C build
+ninja -C build install
