215dbe53c6b90d6375c86ffbee1a706346c9018a
Sebastian Dröge <sebastian@centricular.com>
2020-04-22 21:42:39 +0300

    video: Assert if caption data passed when adding caption metas is empty
    It will cause a critical warning otherwise.

--- gstreamer-video/src/video_meta.rs
+++ gstreamer-video/src/video_meta.rs
@@ -204,6 +204,7 @@ impl VideoCaptionMeta {
         caption_type: ::VideoCaptionType,
         data: &[u8],
     ) -> gst::MetaRefMut<'a, Self, gst::meta::Standalone> {
+        assert!(!data.is_empty());
         unsafe {
             let meta = gst_video_sys::gst_buffer_add_video_caption_meta(
                 buffer.as_mut_ptr(),
