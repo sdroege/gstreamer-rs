f66fcbde9163c5c6f0fcb8851bde9484521f2fba
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2020-04-27 13:12:17 +0200

    ci: use images from gstreamer-rs
    Also:
    - use the same patterns as gstreamer-rs and gst-plugins-rs ci.
    - remove unused cross build template
    - update from Rust 1.36 to 1.40

---
+++ ci/env.sh
@@ -0,0 +1,8 @@
+export RUSTUP_HOME='/usr/local/rustup'
+export PATH=$PATH:/usr/local/cargo/bin
+
+export PKG_CONFIG_PATH=/usr/local/lib/x86_64-linux-gnu/pkgconfig
+export GST_PLUGIN_SYSTEM_PATH=/usr/local/lib/x86_64-linux-gnu/gstreamer-1.0
+export GST_PLUGIN_SCANNER=/usr/local/libexec/gstreamer-1.0/gst-plugin-scanner
+export PATH=$PATH:/usr/local/bin
+export LD_LIBRARY_PATH=/usr/local/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
