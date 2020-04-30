a0fa164b7b39e50b3d901cd89dba0ed0532ab24a
Vivia Nikolaidou <vivia@ahiru.eu>
2020-04-30 18:17:21 +0300

    Update ci gstreamer to master

--- ci/install-gst.sh
+++ ci/install-gst.sh
@@ -1,6 +1,6 @@
 pip3 install meson==0.54.0
 
-git clone --depth 1 https://gitlab.freedesktop.org/gstreamer/gst-build.git --branch 1.16.2
+git clone --depth 1 https://gitlab.freedesktop.org/gstreamer/gst-build.git --branch master
 cd gst-build
 
 meson build -D prefix=/usr/local -D devtools=disabled -D examples=disabled -D gtk_doc=disabled -D introspection=disabled -D libav=disabled -D libnice=disabled -D python=disabled -D ugly=disabled -D vaapi=disabled
