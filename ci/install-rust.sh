9c141a2fbf1e13e76d6255f5f2d240520b2c39fd
Sebastian Dröge <sebastian@centricular.com>
2020-11-01 09:47:18 +0200

    ci: Explicitly select stable Rust version
    And for now we stay at 1.46.0 because 1.47.0 has a bug that prevents
    clippy from succeeding.

--- ci/install-rust.sh
+++ ci/install-rust.sh
@@ -5,6 +5,7 @@ export CARGO_HOME='/usr/local/cargo'
 
 RUSTUP_VERSION=1.22.1
 RUST_VERSION=$1
+RUST_IMAGE_FULL=$2
 RUST_ARCH="x86_64-unknown-linux-gnu"
 
 RUSTUP_URL=https://static.rust-lang.org/rustup/archive/$RUSTUP_VERSION/$RUST_ARCH/rustup-init
@@ -19,7 +20,7 @@ rustup --version
 cargo --version
 rustc --version
 
-if [ "$RUST_VERSION" = "stable" ]; then
+if [ "$RUST_IMAGE_FULL" = "1" ]; then
   rustup component add clippy-preview
   rustup component add rustfmt
   cargo install --force cargo-deny
