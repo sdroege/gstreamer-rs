27ff7b74eb95bda7cdfe769ea34c9689e005135d
Marijn Suijten <marijns95@gmail.com>
2021-01-05 02:20:19 +0100

    gir-files/fix: Do not unnecessarily preserve whitespace
    When deleting elements -P/--pf preserves whitespace in front of the
    elements, creating tons of unnecessary trailing whitespace on
    otherwise-empty lines.

--- gir-files/fix.sh
+++ gir-files/fix.sh
@@ -4,7 +4,7 @@ set -x -e
 # https://github.com/gtk-rs/gir-files/blob/master/reformat.sh
 # `///` used as `//` not works in Windows in this case
 for file in *.gir; do
-    xmlstarlet ed -P -L \
+    xmlstarlet ed -L \
         -d '//_:doc/@line' \
         -d '//_:doc/@filename' \
         -d '///_:source-position' \
@@ -12,7 +12,7 @@ for file in *.gir; do
 done
 
 # replace wayland structures to gpointers
-xmlstarlet ed --pf --inplace \
+xmlstarlet ed --inplace \
     --update '//*[@c:type="wl_display*"]/@c:type' \
     --value gpointer \
     --update '//*[@c:type="wl_registry*"]/@c:type' \
@@ -26,7 +26,7 @@ xmlstarlet ed --pf --inplace \
     GstGLWayland-1.0.gir
 
 # Change X11's Display* and xcb_connection_t* pointers to gpointer
-xmlstarlet ed --pf --inplace \
+xmlstarlet ed --inplace \
     --insert '//_:type[@c:type="Display*"]' \
     --type attr --name 'name' --value 'gpointer' \
     --insert '//_:type[@c:type="xcb_connection_t*"]' \
@@ -38,7 +38,7 @@ xmlstarlet ed --pf --inplace \
     GstGLX11-1.0.gir
 
 # Remove GstMemoryEGL and EGLImage
-xmlstarlet ed --pf --inplace \
+xmlstarlet ed --inplace \
     --delete '//_:record[@name="GLMemoryEGL"]' \
     --delete '//_:record[@name="GLMemoryEGLAllocator"]' \
     --delete '//_:record[@name="GLMemoryEGLAllocatorClass"]' \
@@ -58,45 +58,45 @@ xmlstarlet ed --pf --inplace \
     --delete '//_:function-macro[starts-with(@name, "IS_GL_MEMORY_EGL")]' \
     GstGLEGL-1.0.gir
 
-xmlstarlet ed --pf --inplace \
+xmlstarlet ed --inplace \
     --delete '//_:method[@c:identifier="gst_gl_display_egl_from_gl_display"]' \
     --delete '//_:method[@c:identifier="egl_from_gl_display"]' \
     GstGLEGL-1.0.gir
 
 # Remove all libcheck related API
-xmlstarlet ed --pf --inplace \
+xmlstarlet ed --inplace \
     --delete '//_:function[starts-with(@name, "check_")]' \
     --delete '//_:function[starts-with(@name, "buffer_straw_")]' \
     --delete '//_:callback[starts-with(@name, "Check")]' \
     --delete '//_:record[starts-with(@name, "Check")]' \
     GstCheck-1.0.gir
 # Change GstVideoAncillary.data to a fixed-size 256 byte array
-xmlstarlet ed --pf --inplace \
+xmlstarlet ed --inplace \
     --delete '//_:record[@name="VideoAncillary"]/_:field[@name="data"]/_:array/@length' \
     --delete '//_:record[@name="VideoAncillary"]/_:field[@name="data"]/_:array/@fixed-size' \
     --insert '//_:record[@name="VideoAncillary"]/_:field[@name="data"]/_:array' \
     --type attr --name 'fixed-size' --value '256' \
      GstVideo-1.0.gir
 
-xmlstarlet ed --pf --inplace \
+xmlstarlet ed --inplace \
     --delete "//_:member[@c:identifier=\"GST_VIDEO_BUFFER_FLAG_ONEFIELD\"][2]" \
     --delete "//_:member[@c:identifier=\"GST_VIDEO_FRAME_FLAG_ONEFIELD\"][2]" \
     GstVideo-1.0.gir
 
-xmlstarlet ed --pf --inplace \
+xmlstarlet ed --inplace \
     --delete '//_:record[@name="ISO639LanguageDescriptor"]/_:field[@name="language"]/_:array/@c:type' \
     --insert '//_:record[@name="ISO639LanguageDescriptor"]/_:field[@name="language"]/_:array' \
     --type attr --name 'c:type' --value 'gchar' \
      GstMpegts-1.0.gir
 
-xmlstarlet ed --pf --inplace \
+xmlstarlet ed --inplace \
     --delete '//_:record[@name="MIKEYPayloadKeyData"]/_:field[@name="kv_data"]/_:array/@c:type' \
     --insert '//_:record[@name="MIKEYPayloadKeyData"]/_:field[@name="kv_data"]/_:array' \
     --type attr --name 'c:type' --value 'guint8' \
      GstSdp-1.0.gir
 
 # Remove duplicated enums
-xmlstarlet ed --pf --inplace \
+xmlstarlet ed --inplace \
     --delete '//_:enumeration[@name="EditMode"]/_:member[starts-with(@name, "edit_")]' \
     --delete '//_:enumeration[@name="Edge"]/_:member[starts-with(@name, "edge_")]' \
     GES-1.0.gir
