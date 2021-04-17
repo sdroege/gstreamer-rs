fc1716cd8aea8bdd5b80ab2d46620280709e860b
Marijn Suijten <marijns95@gmail.com>
2021-04-17 12:11:52 +0200

    ci/install-gst: Clone gst-build master branch

--- ci/install-gst.sh
+++ ci/install-gst.sh
@@ -2,7 +2,7 @@ set -e
 
 pip3 install meson==0.57.1
 
-git clone --depth 1 https://gitlab.freedesktop.org/gstreamer/gst-build.git --branch 1.18
+git clone --depth 1 https://gitlab.freedesktop.org/gstreamer/gst-build.git --branch master
 cd gst-build
 
 meson build -D prefix=/usr/local -D devtools=disabled -D examples=disabled -D gtk_doc=disabled -D introspection=disabled -D libav=disabled -D libnice=disabled -D python=disabled -D ugly=disabled -D vaapi=disabled
