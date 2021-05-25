56f6b9e727c649ab62b71c0f341b6d861afc8d2e
Sophie Herold <sophie@hemio.de>
2021-05-25 16:37:04 +0200

    Generate docs artifact for gir-doc

--- gstreamer-audio/src/lib.rs
+++ gstreamer-audio/src/lib.rs
@@ -6,6 +6,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+#![cfg_attr(feature = "dox", feature(doc_cfg))]
+
 extern crate array_init;
 #[macro_use]
 extern crate bitflags;
