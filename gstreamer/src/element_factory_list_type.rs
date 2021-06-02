b0fc500ed1ee408752f13379796f42a2bf62f721
Guillaume Gomez <guillaume1.gomez@gmail.com>
2021-06-02 20:23:29 +0200

    Generate missing doc aliases

--- gstreamer/src/element_factory_list_type.rs
+++ gstreamer/src/element_factory_list_type.rs
@@ -6,27 +6,47 @@ use glib::translate::*;
 bitflags! {
     #[doc(alias = "GstElementFactoryListType")]
     pub struct ElementFactoryListType: u64 {
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_DECODER")]
         const DECODER          = ffi::GST_ELEMENT_FACTORY_TYPE_DECODER as u64;
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_ENCODER")]
         const ENCODER          = ffi::GST_ELEMENT_FACTORY_TYPE_ENCODER as u64;
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_SINK")]
         const SINK             = ffi::GST_ELEMENT_FACTORY_TYPE_SINK as u64;
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_SRC")]
         const SRC              = ffi::GST_ELEMENT_FACTORY_TYPE_SRC as u64;
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_MUXER")]
         const MUXER            = ffi::GST_ELEMENT_FACTORY_TYPE_MUXER as u64;
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_DEMUXER")]
         const DEMUXER          = ffi::GST_ELEMENT_FACTORY_TYPE_DEMUXER as u64;
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_PARSER")]
         const PARSER           = ffi::GST_ELEMENT_FACTORY_TYPE_PARSER as u64;
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_PAYLOADER")]
         const PAYLOADER        = ffi::GST_ELEMENT_FACTORY_TYPE_PAYLOADER as u64;
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_DEPAYLOADER")]
         const DEPAYLOADER      = ffi::GST_ELEMENT_FACTORY_TYPE_DEPAYLOADER as u64;
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_FORMATTER")]
         const FORMATTER        = ffi::GST_ELEMENT_FACTORY_TYPE_FORMATTER as u64;
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_DECRYPTOR")]
         const DECRYPTOR        = ffi::GST_ELEMENT_FACTORY_TYPE_DECRYPTOR as u64;
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_ENCRYPTOR")]
         const ENCRYPTOR        = ffi::GST_ELEMENT_FACTORY_TYPE_ENCRYPTOR as u64;
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_HARDWARE")]
         const HARDWARE         = ffi::GST_ELEMENT_FACTORY_TYPE_HARDWARE as u64;
 
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_MEDIA_VIDEO")]
         const MEDIA_VIDEO      = ffi::GST_ELEMENT_FACTORY_TYPE_MEDIA_VIDEO as u64;
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_MEDIA_AUDIO")]
         const MEDIA_AUDIO      = ffi::GST_ELEMENT_FACTORY_TYPE_MEDIA_AUDIO as u64;
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_MEDIA_IMAGE")]
         const MEDIA_IMAGE      = ffi::GST_ELEMENT_FACTORY_TYPE_MEDIA_IMAGE as u64;
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_MEDIA_SUBTITLE")]
         const MEDIA_SUBTITLE   = ffi::GST_ELEMENT_FACTORY_TYPE_MEDIA_SUBTITLE as u64;
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_MEDIA_METADATA")]
         const MEDIA_METADATA   = ffi::GST_ELEMENT_FACTORY_TYPE_MEDIA_METADATA as u64;
 
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_ANY")]
         const ANY              = ffi::GST_ELEMENT_FACTORY_TYPE_ANY as u64;
+        #[doc(alias = "GST_ELEMENT_FACTORY_TYPE_MEDIA_ANY")]
         const MEDIA_ANY        = ffi::GST_ELEMENT_FACTORY_TYPE_MEDIA_ANY as u64;
 
         const VIDEO_ENCODER    = Self::ENCODER.bits | Self::MEDIA_VIDEO.bits | Self::MEDIA_IMAGE.bits;
