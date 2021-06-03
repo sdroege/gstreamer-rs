sdroege/gstreamer-rs

commit 3f442d97756228b14de3764ebdfab6e69bdda56d

prep for regen: relaxed get fn identification

gstreamer/Gir.toml
@@ -23,9 +23,11 @@ generate = [
     "Gst.CoreError",
     "Gst.DebugColorFlags",
     "Gst.DebugGraphDetails",
+    "Gst.DebugLevel",
     "Gst.EventTypeFlags",
     "Gst.LibraryError",
     "Gst.PadDirection",
+    "Gst.PadMode",
     "Gst.PadPresence",
     "Gst.PadProbeReturn",
     "Gst.ParseError",
@@ -242,10 +244,6 @@ status = "generate"
     [[object.derive]]
     name = "Debug, PartialEq, Eq, PartialOrd, Ord, Hash"
     [[object.function]]
-    name = "get_name"
-    # consumes `self` so not considered as a getter by gir
-    rename = "name"
-    [[object.function]]
     name = "register"
     # Needs manual work
     ignore = true
@@ -357,14 +355,6 @@ status = "generate"
     [[object.member]]
     name = "instant_rate_sync_time"
     version = "1.18"
-    [[object.function]]
-    name = "get_flags"
-    # consumes `self` so not considered as a getter by gir
-    rename = "flags"
-    [[object.function]]
-    name = "get_name"
-    # consumes `self` so not considered as a getter by gir
-    rename = "name"
 
 [[object]]
 name = "Gst.Structure"
@@ -537,6 +527,11 @@ manual_traits = ["ElementExtManual"]
     name = "get_current_clock_time"
     # manual implementation also works for earlier versions
     manual = true
+
+    [[object.function]]
+    name = "get_request_pad"
+    # rename to more explicite name and avoid conflict with `request_pad`
+    rename = "request_pad_simple"
 
     [[object.function]]
     name = "post_message"
@@ -725,14 +720,6 @@ final_type = true
 name = "Gst.TypeFindFactory"
 status = "generate"
 final_type = true
-
-[[object]]
-name = "Gst.DebugLevel"
-status = "generate"
-    [[object.function]]
-    name = "get_name"
-    # consumes `self` so not considered as a getter by gir
-    rename = "name"
 
 [[object]]
 name = "Gst.DeviceProviderFactory"
@@ -1074,14 +1061,6 @@ manual_traits = ["PadExtManual"]
     name = "template"
     # Has a proper getter
     ignore = true
-
-[[object]]
-name = "Gst.PadMode"
-status = "generate"
-    [[object.function]]
-    name = "get_name"
-    # consumes `self` so not considered as a getter by gir
-    rename = "name"
 
 [[object]]
 name = "Gst.PadTemplate"
@@ -1672,8 +1651,6 @@ name = "Gst.StateChange"
 status = "generate"
     [[object.function]]
     name = "get_name"
-    # consumes `self` so not considered as a getter by gir
-    rename = "name"
     # Manual function for < v1_14:
     manual = true
     # Always generate the trait, without version constraint:

commit 9fd768425e2985585d9091e9eb2983f81cd02f81

Rename request_pad_simple to avoid 1.19.1 clash
Fixes: 3f442d977 ("prep for regen: relaxed get fn identification")

gstreamer/Gir.toml
@@ -755,8 +755,11 @@ manual_traits = ["ElementExtManual"]
 
     [[object.function]]
     name = "get_request_pad"
-    # rename to more explicite name and avoid conflict with `request_pad`
+    # rename to more explicit name and avoid conflict with `request_pad`
+    # TODO: request_pad_simple is now _also_ in the 1.20 bindings!
     rename = "request_pad_simple"
+    # Renamed version only available in not(1.20)
+    manual = true
 
     [[object.function]]
     name = "post_message"

gstreamer/src/element.rs
@@ -268,6 +268,12 @@ pub trait ElementExtManual: 'static {
     fn current_running_time(&self) -> crate::ClockTime;
     #[doc(alias = "get_current_clock_time")]
     fn current_clock_time(&self) -> crate::ClockTime;
+
+    #[cfg(not(feature = "v1_20"))]
+    #[cfg_attr(feature = "dox", doc(cfg(not(feature = "v1_20"))))]
+    #[doc(alias = "gst_element_get_request_pad")]
+    #[doc(alias = "get_request_pad")]
+    fn request_pad_simple(&self, name: &str) -> Option<Pad>;
 }
 
 impl<O: IsA<Element>> ElementExtManual for O {
@@ -785,6 +791,16 @@ impl<O: IsA<Element>> ElementExtManual for O {
             clock.time()
         } else {
             crate::CLOCK_TIME_NONE
+        }
+    }
+
+    #[cfg(not(feature = "v1_20"))]
+    fn request_pad_simple(&self, name: &str) -> Option<Pad> {
+        unsafe {
+            from_glib_full(ffi::gst_element_get_request_pad(
+                self.as_ref().to_glib_none().0,
+                name.to_glib_none().0,
+            ))
         }
     }
 }