da61c19b69a6d281c7b7e6c225e4b192c7b0229d
Guillaume Desmottes <guillaume@desmottes.be>
2021-09-29 09:31:22 +0200

    ci: install gst from mono repo

--- ci/install-gst.sh
+++ ci/install-gst.sh
@@ -2,9 +2,14 @@ set -e
 
 pip3 install meson==0.59.1
 
-git clone --depth 1 https://gitlab.freedesktop.org/gstreamer/gst-build.git --branch master
-cd gst-build
+# gstreamer-rs already has a 'gstreamer' directory so don't clone there
+pushd .
+cd ..
+git clone --depth 1 https://gitlab.freedesktop.org/gstreamer/gstreamer.git --branch main
+cd gstreamer
 
 meson build -D prefix=/usr/local -D devtools=disabled -D examples=disabled -D gtk_doc=disabled -D introspection=disabled -D libav=disabled -D libnice=disabled -D python=disabled -D ugly=disabled -D vaapi=disabled
 ninja -C build
 ninja -C build install
+
+popd
