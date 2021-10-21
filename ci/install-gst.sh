fc49a0cfd8c5f614acd1ab1040f7976dd16302b2
Guillaume Desmottes <guillaume.desmottes@onestream.live>
2021-10-21 11:37:27 +0200

    ci: build some plugins
    I'll need those for the tests of a new element I'm writting for
    gst-plugins-rs.

--- ci/install-gst.sh
+++ ci/install-gst.sh
@@ -8,7 +8,10 @@ cd ..
 git clone --depth 1 https://gitlab.freedesktop.org/gstreamer/gstreamer.git --branch main
 cd gstreamer
 
-meson build -D prefix=/usr/local -D devtools=disabled -D examples=disabled -D gtk_doc=disabled -D introspection=disabled -D libav=disabled -D libnice=disabled -D python=disabled -D ugly=disabled -D vaapi=disabled
+# plugins required by tests
+PLUGINS="-D gst-plugins-base:ogg=enabled -D gst-plugins-base:vorbis=enabled -D gst-plugins-base:theora=enabled -D gst-plugins-good:matroska=enabled -D gst-plugins-good:vpx=enabled -D gst-plugins-bad:opus=enabled"
+
+meson build -D prefix=/usr/local -D devtools=disabled -D examples=disabled -D gtk_doc=disabled -D introspection=disabled -D libav=disabled -D libnice=disabled -D python=disabled -D ugly=disabled -D vaapi=disabled $PLUGINS
 ninja -C build
 ninja -C build install
 
