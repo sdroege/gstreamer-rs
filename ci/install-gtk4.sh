e6dc3f5b3ed70068fcb9f20d9b526226ed5ae701
Jordan Petridis <jordan@centricular.com>
2022-03-17 13:03:58 +0200

    ci: Patch the gtk4 build in the CI image so it builds
    wayland-protocols 1.25 subproject fails to build on
    debian 11, but in general switch subprojects to stable
    branches.
    
    Upstream MR:
    https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/4580

--- ci/install-gtk4.sh
+++ ci/install-gtk4.sh
@@ -4,7 +4,8 @@ BRANCH=gtk-4-6
 
 git clone https://gitlab.gnome.org/GNOME/gtk.git --branch $BRANCH --depth=1
 cd gtk
-meson build -D prefix=/usr/local
+git apply ../ci/4580.patch
+meson build -D prefix=/usr/local -Dbuild-tests=false
 ninja -C build
 ninja -C build install
 cd ..
