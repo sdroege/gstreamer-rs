062ceaa00a2a1ab303be2041bc082b273f8794f2
Mathieu Duponchelle <mathieu@centricular.com>
2022-04-07 02:09:44 +0200

    rtp: add bindings for RTPBasePayload
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1008>

--- gstreamer-rtp/src/subclass/mod.rs
+++ gstreamer-rtp/src/subclass/mod.rs
@@ -4,11 +4,19 @@
 
 #[cfg(any(feature = "v1_20", feature = "dox"))]
 #[cfg_attr(feature = "dox", doc(cfg(feature = "v1_20")))]
+mod rtp_base_payload;
+
+#[cfg(any(feature = "v1_20", feature = "dox"))]
+#[cfg_attr(feature = "dox", doc(cfg(feature = "v1_20")))]
 mod rtp_header_extension;
 
 pub mod prelude {
     #[doc(hidden)]
     pub use gst::subclass::prelude::*;
+
+    #[cfg(any(feature = "v1_20", feature = "dox"))]
+    #[cfg_attr(feature = "dox", doc(cfg(feature = "v1_20")))]
+    pub use super::rtp_base_payload::{RTPBasePayloadImpl, RTPBasePayloadImplExt};
 
     #[cfg(any(feature = "v1_20", feature = "dox"))]
     #[cfg_attr(feature = "dox", doc(cfg(feature = "v1_20")))]
