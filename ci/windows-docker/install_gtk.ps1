9a9b7bc5ced6785030339b9292546a00117f2574
Jordan Petridis <jordan@centricular.com>
2022-08-17 17:31:28 +0300

    ci: Install gtk on the windows image
    So we can use it in gst-plugins-rs CI.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1084>

---
+++ ci/windows-docker/install_gtk.ps1
@@ -0,0 +1,29 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
+
+$env:Path += ";C:\gst-install\bin\"
+
+# Download gstreamer and all its subprojects
+git clone -b gtk-4-6 --depth 1 https://gitlab.gnome.org/gnome/gtk.git C:\gtk
+if (!$?) {
+  Write-Host "Failed to clone gtk"
+  Exit 1
+}
+
+Set-Location C:\gtk
+
+$env:MESON_ARGS = ""
+
+Write-Output "Building gtk"
+cmd.exe /C "C:\BuildTools\Common7\Tools\VsDevCmd.bat -host_arch=amd64 -arch=amd64 && meson _build $env:MESON_ARGS && meson compile -C _build && ninja -C _build install"
+
+if (!$?) {
+  Write-Host "Failed to build and install gtk"
+  Exit 1
+}
+
+cd C:\
+cmd /c rmdir /s /q  C:\gtk
+if (!$?) {
+  Write-Host "Failed to remove gtk checkout"
+  Exit 1
+}
