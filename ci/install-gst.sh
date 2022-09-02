b6c8938f1aa2802b1a394993b95610c8cf5b0a6d
Arun Raghavan <arun@asymptotic.io>
2022-09-02 12:11:13 -0400

    ci: Enable libnice while building GStreamer
    This enables webrtcbin as well, which is needed for the whipsink plugin
    in gst-plugins-rs.

--- ci/install-gst.sh
+++ ci/install-gst.sh
@@ -11,7 +11,7 @@ cd gstreamer
 # plugins required by tests
 PLUGINS="-D gst-plugins-base:ogg=enabled -D gst-plugins-base:vorbis=enabled -D gst-plugins-base:theora=enabled -D gst-plugins-good:matroska=enabled -D gst-plugins-good:vpx=enabled -D gst-plugins-bad:opus=enabled"
 
-meson build -D prefix=/usr/local -D examples=disabled -D gtk_doc=disabled -D introspection=disabled -D libav=disabled -D libnice=disabled -D python=disabled -D ugly=disabled -D vaapi=disabled $PLUGINS
+meson build -D prefix=/usr/local -D examples=disabled -D gtk_doc=disabled -D introspection=disabled -D libav=disabled -D python=disabled -D ugly=disabled -D vaapi=disabled $PLUGINS
 ninja -C build
 ninja -C build install
 
