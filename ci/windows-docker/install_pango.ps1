4f2d974aa65225c7f549976a10e900cd17a4611e
Jordan Petridis <jordan@centricular.com>
2022-09-29 03:49:42 +0300

    ci/windows: Install pango explicitly
    Instead of relying on the subproject
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1110>

--- ci/windows-docker/install_pango.ps1
+++ ci/windows-docker/install_pango.ps1
@@ -2,25 +2,25 @@
 
 $env:MESON_ARGS = "--prefix=C:\gst-install\"
 
-# Download gtk and all its subprojects
-git clone -b 4.8.1 --depth 1 https://gitlab.gnome.org/gnome/gtk.git C:\gtk
+# Download pango all its subprojects
+git clone -b main --depth 1 https://gitlab.gnome.org/gnome/pango.git C:\pango
 if (!$?) {
-  Write-Host "Failed to clone gtk"
+  Write-Host "Failed to clone pango"
   Exit 1
 }
 
-Set-Location C:\gtk
+Set-Location C:\pango
 
-Write-Output "Building gtk"
+Write-Output "Building pango"
 cmd.exe /C "C:\BuildTools\Common7\Tools\VsDevCmd.bat -host_arch=amd64 -arch=amd64 && meson _build $env:MESON_ARGS && meson compile -C _build && ninja -C _build install"
 
 if (!$?) {
-  Write-Host "Failed to build and install gtk"
+  Write-Host "Failed to build and install pango"
   Exit 1
 }
 
 cd C:\
-cmd /c rmdir /s /q  C:\gtk
+cmd /c rmdir /s /q  C:\pango
 if (!$?) {
   Write-Host "Failed to remove gtk checkout"
   Exit 1
