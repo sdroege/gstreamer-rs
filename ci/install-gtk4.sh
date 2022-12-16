29fb694f10ddd09752b6ad7cdbc0227ba354b8b1
Jordan Petridis <jordan@centricular.com>
2022-12-16 10:13:43 +0200

    ci/install-{gst, gtk}.sh: reformat to make it more readable
    Split commands and strings to multiple lines
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1176>

--- ci/install-gtk4.sh
+++ ci/install-gtk4.sh
@@ -7,7 +7,10 @@ BRANCH=4.8.2
 git clone https://gitlab.gnome.org/GNOME/gtk.git --branch $BRANCH --depth=1
 cd gtk
 
-meson build -D prefix=/usr/local -Dbuild-tests=false -Dwayland-protocols:tests=false
+meson build \
+    -D prefix=/usr/local \
+    -Dbuild-tests=false  \
+    -Dwayland-protocols:tests=false
 ninja -C build
 ninja -C build install
 cd ..
