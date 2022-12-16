7b3b4f56eef6a247c7a5c2b2932645dd088c21b6
Jordan Petridis <jordan@centricular.com>
2022-12-16 10:13:43 +0200

    ci/install-{gst, gtk}.sh: reformat to make it more readable
    Split commands and strings to multiple lines
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1169>

--- ci/install-gst.sh
+++ ci/install-gst.sh
@@ -5,13 +5,32 @@ pip3 install meson==0.64.1
 # gstreamer-rs already has a 'gstreamer' directory so don't clone there
 pushd .
 cd ..
-git clone --depth 1 https://gitlab.freedesktop.org/gstreamer/gstreamer.git --branch main
+git clone https://gitlab.freedesktop.org/gstreamer/gstreamer.git \
+    --depth 1 \
+    --branch main
+
 cd gstreamer
 
 # plugins required by tests
-PLUGINS="-D gst-plugins-base:ogg=enabled -D gst-plugins-base:vorbis=enabled -D gst-plugins-base:theora=enabled -D gst-plugins-good:matroska=enabled -D gst-plugins-good:vpx=enabled -D gst-plugins-bad:opus=enabled -D gst-plugins-ugly:x264=enabled"
+PLUGINS="-D gst-plugins-base:ogg=enabled \
+         -D gst-plugins-base:vorbis=enabled \
+         -D gst-plugins-base:theora=enabled \
+         -D gst-plugins-good:matroska=enabled \
+         -D gst-plugins-good:vpx=enabled \
+         -D gst-plugins-bad:opus=enabled \
+         -D gst-plugins-ugly:x264=enabled"
 
-meson build -D prefix=/usr/local -D gpl=enabled -D ugly=enabled  -D examples=disabled -D gtk_doc=disabled -D introspection=disabled -D libav=disabled -D python=disabled -D vaapi=disabled $PLUGINS
+meson build \
+    -D prefix=/usr/local \
+    -D gpl=enabled \
+    -D ugly=enabled \
+    -D examples=disabled \
+    -D gtk_doc=disabled \
+    -D introspection=disabled \
+    -D libav=disabled \
+    -D python=disabled \
+    -D vaapi=disabled \
+    $PLUGINS
 ninja -C build
 ninja -C build install
 
