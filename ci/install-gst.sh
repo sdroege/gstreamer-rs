6c7196732e5e82b6e701d2ef47abd43a2fd88638
Jordan Petridis <jordan@centricular.com>
2022-12-16 09:59:11 +0200

    ci: Include gpl plugins when building gst
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1169>

--- ci/install-gst.sh
+++ ci/install-gst.sh
@@ -11,11 +11,14 @@ cd gstreamer
 # plugins required by tests
 PLUGINS="-D gst-plugins-base:ogg=enabled -D gst-plugins-base:vorbis=enabled -D gst-plugins-base:theora=enabled -D gst-plugins-good:matroska=enabled -D gst-plugins-good:vpx=enabled -D gst-plugins-bad:opus=enabled"
 
-meson build -D prefix=/usr/local -D examples=disabled -D gtk_doc=disabled -D introspection=disabled -D libav=disabled -D python=disabled -D ugly=disabled -D vaapi=disabled $PLUGINS
+meson build -D prefix=/usr/local -D gpl=enabled -D examples=disabled -D gtk_doc=disabled -D introspection=disabled -D libav=disabled -D python=disabled -D ugly=disabled -D vaapi=disabled $PLUGINS
 ninja -C build
 ninja -C build install
 
 cd ..
 rm -rf gstreamer/
+
+# Check what plugins we installed
+gst-inspect-1.0
 
 popd
