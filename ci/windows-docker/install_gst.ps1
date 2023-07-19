16e1f924897690f6e9a4905ecea942481ad4468e
Nirbheek Chauhan <nirbheek@centricular.com>
2023-07-19 14:13:50 +0530

    ci: Use meson --vsenv instead of invoking VsDevCmd.bat
    This is simpler, easier to use, and less likely to break.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1298>

--- ci/windows-docker/install_gst.ps1
+++ ci/windows-docker/install_gst.ps1
@@ -14,30 +14,35 @@ Move-Item C:/subprojects/*  C:\gstreamer\subprojects
 Write-Output "Running meson subproject reset"
 meson subprojects update --reset
 
-$env:MESON_ARGS = "--prefix=C:\gst-install\ " +
-    "-Dglib:installed_tests=false " +
-    "-Dlibnice:tests=disabled " +
-    "-Dlibnice:examples=disabled " +
-    "-Dffmpeg:tests=disabled " +
-    "-Dopenh264:tests=disabled " +
-    "-Dpygobject:tests=false " +
-    "-Dgpl=enabled " +
-    "-Dugly=enabled " +
-    "-Dbad=enabled " +
-    "-Dges=enabled " +
-    "-Drtsp_server=enabled " +
-    "-Ddevtools=enabled " +
-    "-Dsharp=disabled " +
-    "-Dpython=disabled " +
-    "-Dlibav=disabled " +
-    "-Dvaapi=disabled " +
-    "-Dgst-plugins-base:pango=enabled " +
-    "-Dgst-plugins-good:cairo=enabled " +
-    "-Dgst-plugins-good:lame=disabled "
+$MESON_ARGS = @(`
+  "--prefix=C:\gst-install", `
+  "-Dglib:installed_tests=false", `
+  "-Dlibnice:tests=disabled", `
+  "-Dlibnice:examples=disabled", `
+  "-Dffmpeg:tests=disabled", `
+  "-Dopenh264:tests=disabled", `
+  "-Dpygobject:tests=false", `
+  "-Dgpl=enabled", `
+  "-Dugly=enabled", `
+  "-Dbad=enabled", `
+  "-Dges=enabled", `
+  "-Drtsp_server=enabled", `
+  "-Ddevtools=enabled", `
+  "-Dsharp=disabled", `
+  "-Dpython=disabled", `
+  "-Dlibav=disabled", `
+  "-Dvaapi=disabled", `
+  "-Dgst-plugins-base:pango=enabled", `
+  "-Dgst-plugins-good:cairo=enabled", `
+  "-Dgst-plugins-good:lame=disabled"
+)
 
-Write-Output "Building gst"
-cmd.exe /C "C:\BuildTools\Common7\Tools\VsDevCmd.bat -host_arch=amd64 -arch=amd64 && meson setup _build $env:MESON_ARGS && meson compile -C _build && meson install -C _build"
 
+Write-Output "Building gstreamer"
+meson setup --vsenv $MESON_ARGS _build
+meson compile -C _build
+# meson install does a spurious rebuild sometimes that then fails
+meson install --no-rebuild -C _build
 
 cd c:\
 Remove-Item -LiteralPath "C:\gstreamer" -Force -Recurse
