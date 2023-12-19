0533160d944fe024cd1b7a0aa4232b0529542732
Sebastian Dröge <sebastian@centricular.com>
2023-12-19 10:38:57 +0200

    rtp: Add support for new `RTPBasePayload::extensions` property
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1368>

--- gstreamer-rtp/src/rtp_base_payload.rs
+++ gstreamer-rtp/src/rtp_base_payload.rs
@@ -1,6 +1,6 @@
 use std::ptr;
 
-use glib::{object::IsA, translate::*};
+use glib::{prelude::*, translate::*};
 
 use crate::RTPBasePayload;
 
@@ -24,6 +24,48 @@ pub trait RTPBasePayloadExtManual: sealed::Sealed + IsA<RTPBasePayload> + 'stati
                         .unwrap_or(ptr::null_mut()),
                 ),
                 "Failed to negotiate by setting outcaps structure"
+            )
+        }
+    }
+
+    #[cfg(feature = "v1_24")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "v1_24")))]
+    fn extensions(&self) -> Vec<crate::RTPHeaderExtension> {
+        let extensions = self.as_ref().property::<gst::Array>("extensions");
+
+        extensions
+            .iter()
+            .map(|v| v.get::<crate::RTPHeaderExtension>().unwrap())
+            .collect()
+    }
+
+    #[cfg(feature = "v1_24")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "v1_24")))]
+    #[doc(alias = "extensions")]
+    fn connect_extensions_notify<F: Fn(&Self) + Send + Sync + 'static>(
+        &self,
+        f: F,
+    ) -> glib::SignalHandlerId {
+        unsafe extern "C" fn notify_extensions_trampoline<
+            P: IsA<RTPBasePayload>,
+            F: Fn(&P) + Send + Sync + 'static,
+        >(
+            this: *mut ffi::GstRTPBasePayload,
+            _param_spec: glib::ffi::gpointer,
+            f: glib::ffi::gpointer,
+        ) {
+            let f: &F = &*(f as *const F);
+            f(RTPBasePayload::from_glib_borrow(this).unsafe_cast_ref())
+        }
+        unsafe {
+            let f: Box<F> = Box::new(f);
+            glib::signal::connect_raw(
+                self.as_ptr() as *mut _,
+                b"notify::extensions\0".as_ptr() as *const _,
+                Some(std::mem::transmute::<_, unsafe extern "C" fn()>(
+                    notify_extensions_trampoline::<Self, F> as *const (),
+                )),
+                Box::into_raw(f),
             )
         }
     }
