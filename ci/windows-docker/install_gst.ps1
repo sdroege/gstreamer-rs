52c764b9868101b6ce36b5904f506bd069994032
Nirbheek Chauhan <nirbheek@centricular.com>
2023-12-19 17:42:18 +0530

    ci: Fix pwsh scripts not exiting on error
    We all know that external utilities returning a non-zero exit code do
    not terminate a powershell script. However, most do not know (and
    neither did I) that it is impossible to promote error exit codes to
    script-terminating errors with ErrorActionPreference.
    
    Explicitly check the return codes and Exit.
    
    https://github.com/MicrosoftDocs/PowerShell-Docs/issues/1583
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1368>

--- ci/windows-docker/install_gst.ps1
+++ ci/windows-docker/install_gst.ps1
@@ -1,9 +1,11 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
-# Make sure powershell exits on errors
-$ErrorActionPreference = "Stop"
 
 # Download gstreamer and all its subprojects
 git clone -b $env:DEFAULT_BRANCH --depth 1 https://gitlab.freedesktop.org/gstreamer/gstreamer.git C:\gstreamer
+if (!$?) {
+  Write-Host "Failed to clone gstreamer"
+  Exit 1
+}
 
 Set-Location C:\gstreamer
 
@@ -13,6 +15,10 @@ Move-Item C:/subprojects/*  C:\gstreamer\subprojects
 # Update the subprojects cache
 Write-Output "Running meson subproject reset"
 meson subprojects update --reset
+if (!$?) {
+  Write-Host "Failed to update gstreamer subprojects"
+  Exit 1
+}
 
 $MESON_ARGS = @(`
   "--prefix=C:\gst-install", `
@@ -42,9 +48,24 @@ echo "subproject('gtk')" >> meson.build
 
 Write-Output "Building gstreamer"
 meson setup --vsenv $MESON_ARGS _build
+if (!$?) {
+  type "_build\meson-logs\meson-log.txt"
+  Write-Host "Failed to run meson setup, see log above"
+  Exit 1
+}
+
+Write-Output "Compiling gstreamer"
 meson compile -C _build
+if (!$?) {
+  Write-Host "Failed to run meson compile"
+  Exit 1
+}
 # meson install does a spurious rebuild sometimes that then fails
 meson install --no-rebuild -C _build
+if (!$?) {
+  Write-Host "Failed to run meson install"
+  Exit 1
+}
 
 cd c:\
 Remove-Item -LiteralPath "C:\gstreamer" -Force -Recurse
