57f407fa899447f1b03c7837e2240ecb1cb960b0
Sebastian Dröge <sebastian@centricular.com>
2024-08-05 12:19:57 +0300

    ci: Also use cargo-c 0.10.3 for Rust 1.80
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1491>

--- ci/install-rust.sh
+++ ci/install-rust.sh
@@ -35,7 +35,7 @@ if [ "$RUST_IMAGE_FULL" = "1" ]; then
   cargo install --locked --force grcov
 fi
 
-if [ "$RUST_VERSION" = "nightly" ]; then
+if [ "$RUST_VERSION" = "nightly" -o "$RUST_VERSION" = "1.80.0" ]; then
     cargo install --locked cargo-c --version 0.10.3+cargo-0.81
 else
     cargo install --locked cargo-c --version 0.9.26+cargo-0.74
