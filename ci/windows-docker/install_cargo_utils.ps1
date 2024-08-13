60363a9344fba6943283e551c69f84cf47d3064a
Sebastian Dröge <sebastian@centricular.com>
2024-08-13 09:33:36 +0300

    ci: Fix version checks
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1506>

--- ci/windows-docker/install_cargo_utils.ps1
+++ ci/windows-docker/install_cargo_utils.ps1
@@ -4,10 +4,10 @@ rustup --version
 rustc --version
 cargo --version
 
-if (("$RUST_VERSION" -eq "nightly") -or ("$RUST_VERSION" -eq "1.80.0" )) {
-    cargo install --locked cargo-c --version 0.10.3+cargo-0.81
-} else {
+if ("$RUST_VERSION" -eq "1.71.1") {
     cargo install --locked cargo-c --version 0.9.26+cargo-0.74
+} else {
+    cargo install --locked cargo-c --version 0.10.3+cargo-0.81
 }
 
 if (!$?) {
