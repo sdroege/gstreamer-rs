101544d3f743a645918c35e689706c426b66166e
Jordan Petridis <jordan@centricular.com>
2024-08-16 20:08:04 +0300

    ci: Fix rust version check in the windows build
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1506>

--- ci/windows-docker/install_cargo_utils.ps1
+++ ci/windows-docker/install_cargo_utils.ps1
@@ -4,7 +4,7 @@ rustup --version
 rustc --version
 cargo --version
 
-if ("$RUST_VERSION" -eq "1.71.1") {
+if ("$env:RUST_VERSION" -eq "1.71.1") {
     cargo install --locked cargo-c --version 0.9.26+cargo-0.74
 } else {
     cargo install --locked cargo-c --version 0.10.3+cargo-0.81
