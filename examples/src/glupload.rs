e5a796ed2eb5c24018c889a633ee63e215728952
Sebastian Dröge <sebastian@centricular.com>
2024-10-19 17:31:47 +0300

    examples: Remove unnecessary mutex
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1563>

--- examples/src/glupload.rs
+++ examples/src/glupload.rs
@@ -11,7 +11,6 @@ use std::{
     mem,
     num::NonZeroU32,
     ptr,
-    sync::Mutex,
 };
 
 use anyhow::{Context, Result};
@@ -478,9 +477,7 @@ impl App {
         .context("Couldn't wrap GL context")?;
 
         let gl_context = shared_context.clone();
-        // FIXME: Once MSRV is 1.72 the Mutex is not necessary anymore because
-        // std::sync::mpsc::Sender is Sync by itself
-        let event_proxy = Mutex::new(event_loop.create_proxy());
+        let event_proxy = event_loop.create_proxy();
 
         #[allow(clippy::single_match)]
         bus.set_sync_handler(move |_, msg| {
@@ -513,7 +510,7 @@ impl App {
                 _ => (),
             }
 
-            if let Err(e) = event_proxy.lock().unwrap().send_event(Message::BusEvent) {
+            if let Err(e) = event_proxy.send_event(Message::BusEvent) {
                 eprintln!("Failed to send BusEvent to event proxy: {e}")
             }
 
