8ba700301d3389df030db3ec417f225270131fb0
Sebastian Dröge <sebastian@centricular.com>
2024-11-15 11:59:22 +0200

    analytics: Manually implement FFI GstTensor because of flexible array member
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1591>

---
+++ gstreamer-analytics/sys/src/manual.rs
@@ -0,0 +1,33 @@
+use gstreamer_sys as gst_sys;
+use libc::size_t;
+
+use crate::{GstTensorDataType, GstTensorDim, GstTensorDimOrder, GstTensorLayout};
+
+#[repr(C)]
+pub struct GstTensor {
+    pub id: glib_sys::GQuark,
+    pub layout: GstTensorLayout,
+    pub data_type: GstTensorDataType,
+    pub batch_size: size_t,
+    pub data: *mut gst_sys::GstBuffer,
+    pub dims_order: GstTensorDimOrder,
+    pub num_dims: size_t,
+    pub dims: [GstTensorDim; 0],
+}
+
+impl ::std::fmt::Debug for GstTensor {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        f.debug_struct(&format!("GstTensor @ {self:p}"))
+            .field("id", &self.id)
+            .field("layout", &self.layout)
+            .field("data_type", &self.data_type)
+            .field("batch_size", &self.batch_size)
+            .field("data", &self.data)
+            .field("dims_order", &self.dims_order)
+            .field("num_dims", &self.num_dims)
+            .field("dims", &unsafe {
+                ::std::slice::from_raw_parts(self.dims.as_ptr(), self.num_dims)
+            })
+            .finish()
+    }
+}
