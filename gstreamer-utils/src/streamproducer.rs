aad6fdfed3ee82b89ce3b614b8846f1e61f225d2
Sebastian Dröge <sebastian@centricular.com>
2025-04-25 14:34:23 +0300

    streamproducer: When discarding a buffer then a new keyframe is necessary
    If configured for requiring keyframes.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1721>

--- gstreamer-utils/src/streamproducer.rs
+++ gstreamer-utils/src/streamproducer.rs
@@ -414,7 +414,7 @@ impl StreamProducer {
                 if consumer.discard.load(atomic::Ordering::SeqCst) {
                     consumer
                         .needs_keyframe
-                        .store(false, atomic::Ordering::SeqCst);
+                        .store(true, atomic::Ordering::SeqCst);
                     return None;
                 }
 
