ba7fee12cde719ff22cf5db84740b86f9c812cb9
Hanna Weiß <hweiss@igalia.com>
2025-07-18 14:25:28 +0200

    vulkan: Fix BufferRef conversion in VulkanFullScreenQuadExtManual
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1748>

--- gstreamer-vulkan/src/vulkan_full_screen_quad.rs
+++ gstreamer-vulkan/src/vulkan_full_screen_quad.rs
@@ -14,7 +14,7 @@ pub trait VulkanFullScreenQuadExtManual:
     sealed::Sealed + IsA<VulkanFullScreenQuad> + 'static
 {
     fn draw_into_output(&self, outbuf: &mut gst::BufferRef) -> Result<(), glib::Error> {
-        let out = unsafe { gst::Buffer::from_glib_none(outbuf.as_ptr()) };
+        let out = unsafe { gst::Buffer::from_glib_borrow(outbuf.as_ptr()) };
         self.set_output_buffer(Some(&out))?;
         let ret = self.draw();
         self.set_output_buffer(None)?;
