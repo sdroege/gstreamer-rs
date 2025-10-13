2ffa51c91bb6f20fedaef7c588e364668956a6c4
Thibault Saunier <tsaunier@igalia.com>
2025-10-13 13:35:08 -0300

    controlbinding: Add property_name() and property_spec() accessor methods
    Provides direct access to the control binding's property name and
    ParamSpec fields through safe getter methods on ControlBindingExtManual
    trait.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1820>

--- gstreamer/src/control_binding.rs
+++ gstreamer/src/control_binding.rs
@@ -2,9 +2,23 @@
 
 use glib::{prelude::*, translate::*};
 
-use crate::{ClockTime, ControlBinding};
+use crate::{ffi, ClockTime, ControlBinding};
 
 pub trait ControlBindingExtManual: IsA<ControlBinding> + 'static {
+    fn property_name(&self) -> &glib::GStr {
+        unsafe {
+            let ptr: *mut ffi::GstControlBinding = self.as_ref().to_glib_none().0;
+            glib::GStr::from_ptr((*ptr).name)
+        }
+    }
+
+    fn property_spec(&self) -> &glib::ParamSpec {
+        unsafe {
+            let ptr: *mut ffi::GstControlBinding = self.as_ref().to_glib_none().0;
+            glib::ParamSpec::from_glib_ptr_borrow(&(*ptr).pspec)
+        }
+    }
+
     #[doc(alias = "get_g_value_array")]
     #[doc(alias = "gst_control_binding_get_g_value_array")]
     fn g_value_array(
