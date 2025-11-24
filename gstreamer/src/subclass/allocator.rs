7c2531865eb824a191b6456d25459fc6a403a8d0
Sebastian Dröge <sebastian@centricular.com>
2025-11-24 18:32:04 +0200

    allocator: Fix subclassing API
    There were multiple problems with the API:
      - `AllocatorImpl::free()` was taking a `Memory` by value but at that
        point the reference count is already 0 so this would lead to a
        double unref and other problems. Use a raw pointer instead.
      - `AllocatorImpl::alloc()` was taking an optional `AllocationParams`
        but they would never be `None`.
      - Also use a raw pointer in `AllocatorImpl::alloc()` for the return
        value for consistency and to make it clearer that this is actually
        unsafe and needs special care.
      - Implementing `AllocatorImpl` wrongly can lead to memory safety
        issues so make it an `unsafe` trait and also make its methods
        `unsafe`.
    
    In addition to fixing all this, also add tests that cover allocation and
    the various memory operations based on something like a
    re-implementation of sysmem.
    
    Fixes https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/issues/569
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1857>

--- gstreamer/src/subclass/allocator.rs
+++ gstreamer/src/subclass/allocator.rs
@@ -2,45 +2,42 @@
 
 use std::ptr;
 
-use glib::{bool_error, prelude::*, subclass::prelude::*, translate::*, BoolError};
+use glib::{prelude::*, subclass::prelude::*, translate::*};
 
 use super::prelude::*;
-use crate::{ffi, AllocationParams, Allocator, Memory};
+use crate::{ffi, AllocationParams, Allocator};
 
-pub trait AllocatorImpl: GstObjectImpl + ObjectSubclass<Type: IsA<Allocator>> {
-    fn alloc(&self, size: usize, params: Option<&AllocationParams>) -> Result<Memory, BoolError> {
+pub unsafe trait AllocatorImpl:
+    GstObjectImpl + ObjectSubclass<Type: IsA<Allocator>>
+{
+    unsafe fn alloc(&self, size: usize, params: &AllocationParams) -> *mut ffi::GstMemory {
         self.parent_alloc(size, params)
     }
 
-    fn free(&self, memory: Memory) {
-        self.parent_free(memory)
+    unsafe fn free(&self, memory: *mut ffi::GstMemory) {
+        unsafe { self.parent_free(memory) }
     }
 }
 
 pub trait AllocatorImplExt: AllocatorImpl {
-    fn parent_alloc(
-        &self,
-        size: usize,
-        params: Option<&AllocationParams>,
-    ) -> Result<Memory, BoolError> {
+    unsafe fn parent_alloc(&self, size: usize, params: &AllocationParams) -> *mut ffi::GstMemory {
         unsafe {
             let data = Self::type_data();
             let parent_class = data.as_ref().parent_class() as *mut ffi::GstAllocatorClass;
 
             if let Some(f) = (*parent_class).alloc {
-                from_glib_full::<*mut ffi::GstMemory, Option<_>>(f(
+                f(
                     self.obj().unsafe_cast_ref::<Allocator>().to_glib_none().0,
                     size,
                     mut_override(params.to_glib_none().0),
-                ))
-                .ok_or_else(|| bool_error!("Allocation failed"))
+                )
             } else {
-                Err(bool_error!("No allocation method on parent class"))
+                ptr::null_mut()
             }
         }
     }
 
-    fn parent_free(&self, memory: Memory) {
+    unsafe fn parent_free(&self, memory: *mut ffi::GstMemory) {
         unsafe {
             let data = Self::type_data();
             let parent_class = data.as_ref().parent_class() as *mut ffi::GstAllocatorClass;
@@ -48,7 +45,7 @@ pub trait AllocatorImplExt: AllocatorImpl {
             if let Some(f) = (*parent_class).free {
                 f(
                     self.obj().unsafe_cast_ref::<Allocator>().to_glib_none().0,
-                    memory.into_glib_ptr(),
+                    memory,
                 )
             }
         }
@@ -73,30 +70,20 @@ unsafe extern "C" fn alloc<T: AllocatorImpl>(
 ) -> *mut ffi::GstMemory {
     let instance = &*(ptr as *mut T::Instance);
     let imp = instance.imp();
-    let instance = imp.obj();
 
-    let params = if params.is_null() {
-        None
-    } else {
-        Some(&*(params as *mut AllocationParams))
-    };
+    let params = &*(params as *mut AllocationParams);
 
     imp.alloc(size, params)
-        .map(|memory| memory.into_glib_ptr())
-        .unwrap_or_else(|error| {
-            error!(crate::CAT_RUST, obj = instance, "{:?}", error);
-
-            ptr::null_mut()
-        })
 }
 
 unsafe extern "C" fn free<T: AllocatorImpl>(
     ptr: *mut ffi::GstAllocator,
     memory: *mut ffi::GstMemory,
 ) {
+    debug_assert_eq!((*memory).mini_object.refcount, 0);
+
     let instance = &*(ptr as *mut T::Instance);
     let imp = instance.imp();
-    let memory = from_glib_full(memory);
 
     imp.free(memory);
 }
@@ -106,25 +93,85 @@ mod tests {
     use super::*;
     use crate::prelude::*;
 
+    // The test allocator below is basically replicating GStreamer's default
+    // sysmem allocator except that the memory allocation is separate from the
+    // memory struct for clarity.
+
     pub mod imp {
+        use glib::translate::*;
+        use std::alloc;
+
         use super::*;
+
+        #[repr(C)]
+        struct Memory {
+            mem: ffi::GstMemory,
+            layout: alloc::Layout,
+            data: *mut u8,
+        }
+
+        const LAYOUT: alloc::Layout = alloc::Layout::new::<Memory>();
 
         #[derive(Default)]
         pub struct TestAllocator;
 
         impl ObjectImpl for TestAllocator {}
         impl GstObjectImpl for TestAllocator {}
-        impl AllocatorImpl for TestAllocator {
-            fn alloc(
-                &self,
-                size: usize,
-                _params: Option<&AllocationParams>,
-            ) -> Result<Memory, BoolError> {
-                Ok(Memory::from_slice(vec![0; size]))
+        unsafe impl AllocatorImpl for TestAllocator {
+            unsafe fn alloc(&self, size: usize, params: &AllocationParams) -> *mut ffi::GstMemory {
+                unsafe {
+                    let Some(maxsize) = size
+                        .checked_add(params.prefix())
+                        .and_then(|s| s.checked_add(params.padding()))
+                    else {
+                        return ptr::null_mut();
+                    };
+                    let Ok(layout) = alloc::Layout::from_size_align(maxsize, params.align() + 1)
+                    else {
+                        return ptr::null_mut();
+                    };
+
+                    let mem = alloc::alloc(LAYOUT) as *mut Memory;
+
+                    let data = alloc::alloc(layout);
+
+                    if params.prefix() > 0
+                        && params.flags().contains(crate::MemoryFlags::ZERO_PREFIXED)
+                    {
+                        ptr::write_bytes(data, 0, params.prefix());
+                    }
+
+                    if params.flags().contains(crate::MemoryFlags::ZERO_PADDED) {
+                        ptr::write_bytes(data.add(params.prefix()).add(size), 0, params.padding());
+                    }
+
+                    ffi::gst_memory_init(
+                        ptr::addr_of_mut!((*mem).mem),
+                        params.flags().into_glib(),
+                        self.obj().as_ptr() as *mut ffi::GstAllocator,
+                        ptr::null_mut(),
+                        maxsize,
+                        params.align(),
+                        params.prefix(),
+                        size,
+                    );
+                    ptr::write(ptr::addr_of_mut!((*mem).layout), layout);
+                    ptr::write(ptr::addr_of_mut!((*mem).data), data);
+
+                    mem as *mut ffi::GstMemory
+                }
             }
 
-            fn free(&self, memory: Memory) {
-                self.parent_free(memory)
+            unsafe fn free(&self, mem: *mut ffi::GstMemory) {
+                unsafe {
+                    let mem = mem as *mut Memory;
+
+                    if (*mem).mem.parent.is_null() {
+                        alloc::dealloc((*mem).data, (*mem).layout);
+                        ptr::drop_in_place(ptr::addr_of_mut!((*mem).layout));
+                    }
+                    alloc::dealloc(mem as *mut u8, LAYOUT);
+                }
             }
         }
 
@@ -133,6 +180,117 @@ mod tests {
             const NAME: &'static str = "TestAllocator";
             type Type = super::TestAllocator;
             type ParentType = Allocator;
+
+            fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
+                static ALLOCATOR_TYPE: &[u8] = b"TestAllocatorMemory\0";
+
+                unsafe {
+                    let allocator = obj.as_ptr() as *mut ffi::GstAllocator;
+
+                    // TODO: This should all be in some kind of trait ideally
+                    (*allocator).mem_type = ALLOCATOR_TYPE.as_ptr() as *const _;
+                    (*allocator).mem_map = Some(TestAllocator::mem_map);
+                    (*allocator).mem_unmap = Some(TestAllocator::mem_unmap);
+                    // mem_copy not set because the fallback already does the right thing
+                    (*allocator).mem_share = Some(TestAllocator::mem_share);
+                    (*allocator).mem_is_span = Some(TestAllocator::mem_is_span);
+                }
+            }
+        }
+
+        impl TestAllocator {
+            unsafe extern "C" fn mem_map(
+                mem: *mut ffi::GstMemory,
+                _maxsize: usize,
+                _flags: ffi::GstMapFlags,
+            ) -> glib::ffi::gpointer {
+                let mem = mem as *mut Memory;
+
+                let parent = if (*mem).mem.parent.is_null() {
+                    mem
+                } else {
+                    (*mem).mem.parent as *mut Memory
+                };
+
+                // `(*mem).offset` is added to the pointer by `gst_memory_map()`
+                (*parent).data as *mut _
+            }
+
+            unsafe extern "C" fn mem_unmap(_mem: *mut ffi::GstMemory) {}
+
+            unsafe extern "C" fn mem_share(
+                mem: *mut ffi::GstMemory,
+                offset: isize,
+                size: isize,
+            ) -> *mut ffi::GstMemory {
+                let mem = mem as *mut Memory;
+
+                // Basically a re-implementation of _sysmem_share()
+
+                let parent = if (*mem).mem.parent.is_null() {
+                    mem
+                } else {
+                    (*mem).mem.parent as *mut Memory
+                };
+
+                // Offset and size are actually usizes and the API assumes that negative values simply wrap
+                // around, so let's cast to usizes here and do wrapping arithmetic.
+                let offset = offset as usize;
+                let mut size = size as usize;
+
+                let new_offset = (*mem).mem.offset.wrapping_add(offset);
+                debug_assert!(new_offset < (*mem).mem.maxsize);
+
+                if size == usize::MAX {
+                    size = (*mem).mem.size.wrapping_sub(offset);
+                }
+                debug_assert!(new_offset <= usize::MAX - size);
+                debug_assert!(new_offset + size <= (*mem).mem.maxsize);
+
+                let sub = alloc::alloc(LAYOUT) as *mut Memory;
+
+                ffi::gst_memory_init(
+                    sub as *mut ffi::GstMemory,
+                    (*mem).mem.mini_object.flags | ffi::GST_MINI_OBJECT_FLAG_LOCK_READONLY,
+                    (*mem).mem.allocator,
+                    parent as *mut ffi::GstMemory,
+                    (*mem).mem.maxsize,
+                    (*mem).mem.align,
+                    new_offset,
+                    size,
+                );
+                // This is never actually accessed
+                ptr::write(ptr::addr_of_mut!((*sub).data), ptr::null_mut());
+
+                sub as *mut ffi::GstMemory
+            }
+
+            unsafe extern "C" fn mem_is_span(
+                mem1: *mut ffi::GstMemory,
+                mem2: *mut ffi::GstMemory,
+                offset: *mut usize,
+            ) -> glib::ffi::gboolean {
+                let mem1 = mem1 as *mut Memory;
+                let mem2 = mem2 as *mut Memory;
+
+                // Same parent is checked by `gst_memory_is_span()` already
+                let parent1 = (*mem1).mem.parent as *mut Memory;
+                let parent2 = (*mem2).mem.parent as *mut Memory;
+                debug_assert_eq!(parent1, parent2);
+
+                if !offset.is_null() {
+                    // Offset that can be used on the parent memory to create a
+                    // shared memory that starts with `mem1`.
+                    //
+                    // This needs to use wrapping arithmetic too as in `mem_share()`.
+                    *offset = (*mem1).mem.offset.wrapping_sub((*parent1).mem.offset);
+                }
+
+                // Check if both memories are contiguous.
+                let is_span = ((*mem1).mem.offset + ((*mem1).mem.size)) == (*mem2).mem.offset;
+
+                is_span.into_glib()
+            }
         }
     }
 
@@ -171,5 +329,71 @@ mod tests {
         let memory = allocator.alloc(SIZE, None).unwrap();
 
         assert_eq!(memory.size(), SIZE);
+    }
+
+    #[test]
+    fn test_allocator_mem_ops() {
+        crate::init().unwrap();
+
+        let data = [0, 1, 2, 3, 4, 5, 6, 7];
+
+        let allocator = TestAllocator::default();
+
+        let mut memory = allocator.alloc(data.len(), None).unwrap();
+        assert_eq!(memory.size(), data.len());
+
+        {
+            let memory = memory.get_mut().unwrap();
+            let mut map = memory.map_writable().unwrap();
+            map.copy_from_slice(&data);
+        }
+
+        let copy = memory.copy();
+        assert!(copy.parent().is_none());
+
+        {
+            let map1 = memory.map_readable().unwrap();
+            let map2 = copy.map_readable().unwrap();
+            assert_eq!(map1.as_slice(), map2.as_slice());
+        }
+
+        let share = memory.share(..);
+        assert_eq!(share.parent().unwrap().as_ptr(), memory.as_ptr());
+
+        {
+            let map1 = memory.map_readable().unwrap();
+            let map2 = share.map_readable().unwrap();
+            assert_eq!(map1.as_slice(), map2.as_slice());
+        }
+
+        let sub1 = memory.share(..2);
+        assert_eq!(sub1.size(), 2);
+        assert_eq!(sub1.parent().unwrap().as_ptr(), memory.as_ptr());
+
+        {
+            let map = sub1.map_readable().unwrap();
+            assert_eq!(map.as_slice(), &data[..2]);
+        }
+
+        let sub2 = memory.share(2..);
+        assert_eq!(sub2.size(), 6);
+        assert_eq!(sub2.parent().unwrap().as_ptr(), memory.as_ptr());
+
+        {
+            let map = sub2.map_readable().unwrap();
+            assert_eq!(map.as_slice(), &data[2..]);
+        }
+
+        let offset = sub1.is_span(&sub2).unwrap();
+        assert_eq!(offset, 0);
+
+        let sub3 = sub2.share(2..);
+        assert_eq!(sub3.size(), 4);
+        assert_eq!(sub3.parent().unwrap().as_ptr(), memory.as_ptr());
+
+        {
+            let map = sub3.map_readable().unwrap();
+            assert_eq!(map.as_slice(), &data[4..]);
+        }
     }
 }
