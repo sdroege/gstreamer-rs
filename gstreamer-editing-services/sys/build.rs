87a5ed376bc97d60c4717c141b158c83c60b8e10
Sebastian Dröge <sebastian@centricular.com>
2025-11-26 11:44:31 +0200

    Regenerate with latest gir
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1858>

--- gstreamer-editing-services/sys/build.rs
+++ gstreamer-editing-services/sys/build.rs
@@ -3,16 +3,14 @@
 // from gst-gir-files (https://gitlab.freedesktop.org/gstreamer/gir-files-rs.git)
 // DO NOT EDIT
 
-#[cfg(not(docsrs))]
-use std::process;
-
-#[cfg(docsrs)]
-fn main() {} // prevent linking libraries to avoid documentation failure
-
-#[cfg(not(docsrs))]
 fn main() {
+    if std::env::var("DOCS_RS").is_ok() {
+        // prevent linking libraries to avoid documentation failure
+        return;
+    }
+
     if let Err(s) = system_deps::Config::new().probe() {
         println!("cargo:warning={s}");
-        process::exit(1);
+        std::process::exit(1);
     }
 }
