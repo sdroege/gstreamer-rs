d453295f910f727306c8e078e02d8aca147305ee
Sebastian Dröge <sebastian@centricular.com>
2026-02-05 15:51:31 +0200

    vulkan: Remove once_cell dependency
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1912>

--- gstreamer-vulkan/src/caps_features.rs
+++ gstreamer-vulkan/src/caps_features.rs
@@ -2,9 +2,9 @@
 
 use crate::ffi;
 use gst::CapsFeatures;
-use once_cell::sync::Lazy;
+use std::sync::LazyLock;
 
 pub static CAPS_FEATURE_MEMORY_VULKAN_IMAGE: &glib::GStr =
     unsafe { glib::GStr::from_utf8_with_nul_unchecked(ffi::GST_CAPS_FEATURE_MEMORY_VULKAN_IMAGE) };
-pub static CAPS_FEATURES_MEMORY_VULKAN_IMAGE: Lazy<CapsFeatures> =
-    Lazy::new(|| CapsFeatures::new([CAPS_FEATURE_MEMORY_VULKAN_IMAGE]));
+pub static CAPS_FEATURES_MEMORY_VULKAN_IMAGE: LazyLock<CapsFeatures> =
+    LazyLock::new(|| CapsFeatures::new([CAPS_FEATURE_MEMORY_VULKAN_IMAGE]));
