543017b0e055b97e2c700f66cc3aaf21342d82d7
Edward Hervey <bilboed@bilboed.com>
2026-03-31 14:31:32 +0200

    gstreamer-mpegts: Add more API
    * Section::new(pid, data). To wrap external section data
    * event_new_mpegts_section(section). To be able to convert them to GStreamer
      events
    * event_parse_mpegts_section(event). To be able to recover a Section from a
      GStreamer event
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1968>

--- gstreamer-mpegts/src/lib.rs
+++ gstreamer-mpegts/src/lib.rs
@@ -5,6 +5,8 @@
 use std::sync::Once;
 
 pub use glib;
+#[cfg(feature = "v1_20")]
+use glib::translate::ToGlibPtr;
 pub use gst;
 pub use gstreamer_mpegts_sys as ffi;
 
@@ -27,3 +29,61 @@ pub fn init() {
 mod auto;
 #[allow(unused_imports)]
 pub use crate::auto::*;
+
+#[cfg(feature = "v1_20")]
+#[doc(alias = "gst_event_new_mpegts_section")]
+pub fn event_new_mpegts_section(section: &Section) -> gst::Event {
+    assert_initialized_main_thread!();
+
+    unsafe {
+        let event = ffi::gst_event_new_mpegts_section(section.to_glib_none().0);
+        glib::translate::from_glib_full(event)
+    }
+}
+
+// rustdoc-stripper-ignore-next
+/// Parses a #Section from a #Event.
+///
+/// # Arguments
+///
+/// * `event` - The #Event to parse
+///
+/// # Returns
+///
+/// A `Section` if the event contains MPEG-TS section data, else `None`
+#[cfg(feature = "v1_20")]
+#[doc(alias = "gst_event_parse_mpegts_section")]
+pub fn event_parse_mpegts_section(event: &gst::Event) -> Option<Section> {
+    assert_initialized_main_thread!();
+
+    unsafe {
+        let section = ffi::gst_event_parse_mpegts_section(event.to_glib_none().0);
+        glib::translate::from_glib_full(section)
+    }
+}
+
+#[cfg(feature = "v1_20")]
+impl Section {
+    // rustdoc-stripper-ignore-next
+    /// Creates a new #Section from the provided data.
+    ///
+    /// # Arguments
+    ///
+    /// * `pid` - The PID to which this section belongs
+    /// * `data` - A slice containing the section data (must start with table_id)
+    ///
+    /// # Returns
+    ///
+    /// A new `Section` if the data was valid, else `None`
+    /// ```
+    #[doc(alias = "gst_mpegts_section_new")]
+    pub fn new(pid: u16, data: &[u8]) -> Option<Section> {
+        assert_initialized_main_thread!();
+        unsafe {
+            let len = data.len();
+            let ptr = data.to_glib_full();
+            let section = ffi::gst_mpegts_section_new(pid, ptr, len);
+            glib::translate::from_glib_full(section)
+        }
+    }
+}
