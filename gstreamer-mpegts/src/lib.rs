45b8529bb0cc3c83191d63dff4104b3153afaf27
Edward Hervey <bilboed@bilboed.com>
2026-04-22 16:58:36 +0200

    gstreamer_mpegts: Add docsrs cfg feature
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/1982>

--- gstreamer-mpegts/src/lib.rs
+++ gstreamer-mpegts/src/lib.rs
@@ -31,6 +31,7 @@ mod auto;
 pub use crate::auto::*;
 
 #[cfg(feature = "v1_20")]
+#[cfg_attr(docsrs, doc(cfg(feature = "v1_20")))]
 #[doc(alias = "gst_event_new_mpegts_section")]
 pub fn event_new_mpegts_section(section: &Section) -> gst::Event {
     assert_initialized_main_thread!();
@@ -42,6 +43,7 @@ pub fn event_new_mpegts_section(section: &Section) -> gst::Event {
 }
 
 #[cfg(feature = "v1_20")]
+#[cfg_attr(docsrs, doc(cfg(feature = "v1_20")))]
 #[doc(alias = "gst_message_new_mpegts_section")]
 pub fn message_new_mpegts_section(parent: &gst::Object, section: &Section) -> gst::Message {
     assert_initialized_main_thread!();
@@ -64,6 +66,7 @@ pub fn message_new_mpegts_section(parent: &gst::Object, section: &Section) -> gs
 ///
 /// A `Section` if the event contains MPEG-TS section data, else `None`
 #[cfg(feature = "v1_20")]
+#[cfg_attr(docsrs, doc(cfg(feature = "v1_20")))]
 #[doc(alias = "gst_event_parse_mpegts_section")]
 pub fn event_parse_mpegts_section(event: &gst::Event) -> Option<Section> {
     assert_initialized_main_thread!();
@@ -80,6 +83,7 @@ pub fn event_parse_mpegts_section(event: &gst::Event) -> Option<Section> {
 /// Returns a `Section` if the message contains MPEG-TS section data, else
 /// `None`
 #[cfg(feature = "v1_20")]
+#[cfg_attr(docsrs, doc(cfg(feature = "v1_20")))]
 #[doc(alias = "gst_message_parse_mpegts_section")]
 pub fn message_parse_mpegts_section(message: &gst::Message) -> Option<Section> {
     assert_initialized_main_thread!();
