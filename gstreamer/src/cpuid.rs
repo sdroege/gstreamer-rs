d9d2f455d94fd807fa9d4bb3662366b33b83a965
Taruntej Kanakamalla <tarun@centricular.com>
2026-07-09 17:44:33 +0530

    gst: cpuid: add alias for riscv support function
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/2022>

--- gstreamer/src/cpuid.rs
+++ gstreamer/src/cpuid.rs
@@ -2,10 +2,11 @@
 
 pub use crate::auto::functions::{
     cpuid_supports_arm_neon as supports_arm_neon, cpuid_supports_arm_neon64 as supports_arm_neon64,
-    cpuid_supports_x86_3dnow as supports_x86_3dnow, cpuid_supports_x86_avx as supports_x86_avx,
-    cpuid_supports_x86_avx2 as supports_x86_avx2, cpuid_supports_x86_mmx as supports_x86_mmx,
-    cpuid_supports_x86_mmxext as supports_x86_mmxext, cpuid_supports_x86_sse2 as supports_x86_sse2,
-    cpuid_supports_x86_sse3 as supports_x86_sse3, cpuid_supports_x86_sse4_1 as supports_x86_sse4_1,
+    cpuid_supports_riscv_v as supports_riscv_v, cpuid_supports_x86_3dnow as supports_x86_3dnow,
+    cpuid_supports_x86_avx as supports_x86_avx, cpuid_supports_x86_avx2 as supports_x86_avx2,
+    cpuid_supports_x86_mmx as supports_x86_mmx, cpuid_supports_x86_mmxext as supports_x86_mmxext,
+    cpuid_supports_x86_sse2 as supports_x86_sse2, cpuid_supports_x86_sse3 as supports_x86_sse3,
+    cpuid_supports_x86_sse4_1 as supports_x86_sse4_1,
     cpuid_supports_x86_sse4_2 as supports_x86_sse4_2,
     cpuid_supports_x86_ssse3 as supports_x86_ssse3,
 };
